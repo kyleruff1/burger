2e71fb86555f8e2413eb29bfb4629f9e0114c575
Kyle Ruff <kyleruff@gmail.com>
2019-08-18 09:53:55 -0600

    fixed connection.js added blank table to db

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Passw00rd",
     database: "burgers_db"
   });
 };
