9b8fdd8d617002f26dfd710c238e96eedee791ee
Kyle Ruff <kyleruff@gmail.com>
2019-08-19 17:41:59 -0600

    npm env connection file update

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,25 @@
-var mysql = require("mysql");
-
+    // Global
+const mysql = require("mysql");
+// Connect
 var connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Passw00rd",
-    database: "burgers_db"
-  });
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
 };
-
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
-
-// Export connection for our ORM to use.
+// Export Connection
 module.exports = connection;
